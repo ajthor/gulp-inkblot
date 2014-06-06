3293a20d29c52f2d6071297762fa9371c87abf66
Adam <adam.thorpe.g@gmail.com>
2014-06-06 09:18:49 -0600

    create new blank spec

---
+++ test/index.spec.js
@@ -0,0 +1,36 @@
+/* global describe, it, beforeEach, afterEach, before, after */
+/* global expect, should, assert, require */
+
+'use strict';
+
+var chai = require('chai'),
+    expect = chai.expect,
+    assert = chai.assert,
+    should = chai.should();
+
+describe('exported object', function() {
+
+    var exported = require('/Users/adam/Dropbox/gulp-inkblot/index.js');
+
+    it('should not be undefined', function() {
+        expect(exported).to.not.be.undefined;
+    });
+
+    describe('indexJs function', function() {
+
+        var indexJs = exported;
+
+        it('should exist', function() {
+            expect(indexJs).to.exist;
+        });
+
+        it('should load', function() {});
+
+        it('should scaffold a demo file', function() {});
+
+        it('should extract unit tests from a file', function() {});
+        it('should delete unit tests from a file', function() {});
+        it('should create JSON file if proper option is set', function() {});
+
+    });
+});
