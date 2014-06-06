f1c7cbdc4688fb154d9bfbaa72d67fd62205be4e
Adam <adam.thorpe.g@gmail.com>
2014-06-06 13:50:28 -0600

    refactor: change inkblot compile call to be async

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ module.exports = function (options) {
 	var ib = new inkblot(options);
 
 	return through.obj(function (file, enc, callback) {
+		var ext = path.extname(file.path);
+		var base = path.basename(file.path, ext);
+
 		if (file.isNull()) {
 			this.push(file);
 			return callback();
@@ -30,27 +33,32 @@ module.exports = function (options) {
 		options = options || {};
 
 		try {
-			(function () {
-				var ext = path.extname(file.path);
-				var base = path.basename(file.path, ext);
 
-				ib.compile({
-					cwd: file.cwd,
-					path: file.path,
-					ext: ext,
-					base: base,
-					name: path.basename(file.path),
-					spec: path.join(ib.options.out, base + '.spec' + ext),
-					_contents: file._contents
-				});
+			ib.compile({
+				cwd: file.cwd,
+				path: file.path,
+				ext: ext,
+				base: base,
+				name: path.basename(file.path),
+				spec: path.join(ib.options.out, base + '.spec' + ext),
+				_contents: file._contents
+			}, 
+			function (err) {
+				console.log(err);
+				if (err) {
+					this.emit('error', new gutil.PluginError('gulp-inkblot', err));						
+				}
 
-			})();
+				this.push(file);
+				return callback();
+
+			}.bind(this));
 
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-inkblot', 'Streaming not supported.'));
+			this.push(file);
+			return callback();
 		}
 
-		this.push(file);
-		return callback();
 	});
 };
